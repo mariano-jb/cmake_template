45ebddf8109fbd355e54dc32cf51e2d0c3cb1b78
Mariano Jimenez Brenes <marianojb7@gmail.com>
2019-05-09 09:28:56 -0600

    Adding files required

---
+++ Drivers/STM32F4xx_HAL_Driver/Src/CMakeFiles/HAL_LIBRARY.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_adc.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_adc_ex.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_cortex.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_dma.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_dma_ex.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_flash.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_flash_ex.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_flash_ramfunc.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_gpio.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_i2c.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_i2c_ex.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_pwr.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_pwr_ex.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_rcc.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_rcc_ex.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_rtc.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_rtc_ex.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_sd.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_tim.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_tim_ex.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_hal_uart.c.o"
+  "CMakeFiles/HAL_LIBRARY.dir/stm32f4xx_ll_sdmmc.c.o"
+  "libHAL_LIBRARY.pdb"
+  "libHAL_LIBRARY.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HAL_LIBRARY.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
